1de31f5e18ab43c04dddc94d8c4f77fb707a921f
Nicholas Ferreira Araujo <nickferaa@gmail.com>
2022-07-05 00:21:12 -0300

    Db port change, swagger mod declaration

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import getLogLevels from './common/utils/getLogLevels';
 
@@ -12,6 +13,14 @@ async function bootstrap() {
     allowedHeaders: 'Content-Type,Authorization',
   });
   app.useLogger(getLogLevels(process.env.NODE_ENV === 'production'));
+
+  const config = new DocumentBuilder()
+    .setTitle('Fluxxo API')
+    .build();
+  
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.SERVER_PORT);
 }
 bootstrap();
